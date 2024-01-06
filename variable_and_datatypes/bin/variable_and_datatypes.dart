b0c2e004015303a09abbc9f618cff05a9e936c53
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-01-06 20:33:28 +0100

    initial commit

---
+++ variable_and_datatypes/bin/variable_and_datatypes.dart
@@ -0,0 +1,5 @@
+import 'package:variable_and_datatypes/variable_and_datatypes.dart' as variable_and_datatypes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${variable_and_datatypes.calculate()}!');
+}
