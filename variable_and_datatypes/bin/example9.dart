c4587761723afd05ccfe1820adcf4cb4ad3d222d
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-01-10 10:28:46 +0100

    Worked with a stream in two ways

--- variable_and_datatypes/bin/example9.dart
+++ variable_and_datatypes/bin/example9.dart
@@ -1,4 +1,15 @@
-void main(List<String> arguments) {
-  final you = ['Marach', 'David', 'Emmanuel', 'Marach', 'David', 'Esther', 'Marach'];
-
+void main(List<String> arguments) async{
+  // await for(final i in getNumbers()){
+  //   print(i);
+  // }
+  await for(final marach in getStream()){print(marach);}
 }
+
+// Stream<int> getNumbers() async*{
+//   for(int i = 0; i < 10; i++){
+//     await Future.delayed(const Duration(seconds: 1));
+//     yield i;
+//   }
+// }
+
+Stream<int> getStream() => Stream.periodic(const Duration(seconds: 1), (value) => value).take(20);
